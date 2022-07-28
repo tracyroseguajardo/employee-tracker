a5aef5d4d025adfa1daa0e8e7569fd438b263ce7
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-07-28 15:58:04 -0700

    set up server.js and schema

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS personelle_db;
+CREATE DATABASE personelle_db;
+
+USE personelle_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id),
+  REFERENCES department(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
+  FOREIGN KEY (role_id),
+  REFERENCES role(id),
+  PRIMARY KEY id,
+  FOREIGN KEY (manager_id),
+  REFERENCES employee(id),
+  ON DELETE SET NULL
+);
---
+++ server.js
@@ -0,0 +1,50 @@
+// Packages
+const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+const path = require("path")
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'classlist_db'
+    },
+    console.log(`Connected to the classlist_db database.`)
+  );
+
+  const menuQuestion =
+  {
+      type: "list",
+      message: "What would you like to do?",
+      name: "action",
+      choices: ["view all departments", "add a department", "add a role", "add an employee", "update an employee role"]
+  }
+
+  function menu() {
+    inquirer.prompt(menuQuestion).then((answer) => {
+        switch (answer.action) {
+            case "view all departments":
+                console.log("chose view all departments");
+                break;
+            case "add a department":
+                console.log("chose add a department");
+                break;
+            case "add a role":
+                console.log("chose add a role");
+                break; 
+            case "add an employee":
+                console.log("chose add an employee");
+                break;           
+            case "update an employee role":
+                console.log("chose update an employeerole");
+                break;
+        }
+    })
+}
